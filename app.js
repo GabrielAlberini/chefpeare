013679f2da25019d6feb6409d30e2e3696288200
CuerpoCasa <gabialberini733@gmail.com>
2021-12-23 12:02:45 -0300

    2.1

--- app.js
+++ app.js
@@ -29,9 +29,7 @@
 
 })(document);
 
-
 /*Owl Carousel whit JQuery*/
-
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:20,
@@ -52,50 +50,60 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+/*Carga previa a mostrar contenido de página*/
 window.onload = function(){
     $("#preload").fadeOut();
     $("body").removeClass("hidden");
 };
 
 /*ContactForm*/
+//Se declaran anteponiendo el $ porque 
+//están haciendo referencia a un elemento del DOM
+// ((d) => {
+//     //Se declaran anteponiendo el $ porque 
+//     //están haciendo referencia a un elemento del DOM
+//     const $form = d.querySelector(".contact-form"),
+//         $loader = d.querySelector(".contact-form-loader"),
+//         $response = d.querySelector(".contact-form-response");  
 
-    //Se declaran anteponiendo el $ porque 
-    //están haciendo referencia a un elemento del DOM
+//         $form.addEventListener("submit", e=> {
+
+//             e.preventDefault();
+//             $loader.classList.remove("none");
+//             fetch("https://formsubmit.co/ajax/gabialberini733@gmail.com", {
+//                 method: "POST",
+//                 body: new FormData(e.target)
+//             }).then((res)=> (res.ok ? res.json() : Promise.reject(res)))
+//             .then(json =>{
+//                 console.log(json)
+//                 location.hash = "#gracias"
+//                 $form.reset();
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//                 let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente"
+//                 $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`
+//             }).finally(()=> {
+//                 $loader.classList.add("none")
+//                 setTimeout(() => {
+//                     location.hash = "#close"
+//                 }, 1500)
+//             });   
+//         });
+
+// })(document)
+
+ ((d) => {
     
-
-((d) => {
-    //Se declaran anteponiendo el $ porque 
-    //están haciendo referencia a un elemento del DOM
     const $form = d.querySelector(".contact-form"),
         $loader = d.querySelector(".contact-form-loader"),
-        $response = d.querySelector(".contact-form-response");  
+        $response = d.querySelector(".contacto-form-response");
 
-        $form.addEventListener("submit", e=> {
+    $form.addEventListener('submit', e=> {
+        e.preventDefault();
+        $loader.classList.remove("none");
+        fetch("https://formsubmit.co/ajax/chefpeare@gmail.com")
+    })
 
-            e.preventDefault();
-            $loader.classList.remove("none");
-            fetch("https://formsubmit.co/ajax/gabialberini733@gmail.com", {
-                method: "POST",
-                body: new FormData(e.target)
-            }).then((res)=> (res.ok ? res.json() : Promise.reject(res)))
-            .then(json =>{
-                console.log(json)
-                location.hash = "#gracias"
-                $form.reset();
-            })
-            .catch(err => {
-                console.log(err)
-                let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente"
-                $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`
-            }).finally(()=> {
-                $loader.classList.add("none")
-                setTimeout(() => {
-                    location.hash = "#close"
-                }, 1500)
-            });   
-        });
-
-})(document)
-
-//Cambio de lenguaje
+ })(document);
 
