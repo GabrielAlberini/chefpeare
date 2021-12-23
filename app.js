f1c1d05e84406367ca62e9a0b5343f97e6677b8e
CuerpoCasa <gabialberini733@gmail.com>
2021-12-23 15:11:07 -0300

    V2.2

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*Carga previa a mostrar contenido de página*/
+window.onload = function(){
+    $("#preload").fadeOut();
+    $("body").removeClass("hidden");
+};
+
 /*Menu y botón cambio de idioma*/
 ((d) => {
     //Se declaran anteponiendo el $ porque 
@@ -17,16 +23,53 @@
         $btnMenu.firstElementChild.classList.remove("none");
         $btnMenu.lastElementChild.classList.add("none");
         $menu.classList.remove("is-active")
-    });
+    });    
 
-    //Cambio de idioma
+})(document);
+
+/*Contact Form*/
+((d) => {
+
+const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contacto-form-response");
+
+    $form.addEventListener('submit', e=> {
+    e.preventDefault();
+    $loader.classList.remove("none");
+    fetch("https://formsubmit.co/ajax/chefpeare@gmail.com", {
+        method: "POST",
+        body: new FormData(e.target),
+    }).then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then(json=> {
+        console.log(json);
+        location.hash = "#gracias";
+        $form.reset()
+    })
+    .catch(err => {
+        console.log(err)
+        let message = err.status.text || "Ocurrio un error al enviar, intenta nuevamente."
+        $response.querySelector("h3").innetHTML = `Error ${err.status}:${message}`
+    })
+    .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(()=> {
+            location.hash = "#close"
+        }, 3000)
+    })
+})
+})(document);
+
+//Cambio de idioma
+((d)=> {
+    const $check = d.querySelector(".check")
+
     $check.addEventListener("click", () => {
         let checkValue = $check.checked;
         checkValue 
         ? location.href="leng/index.html"
         : location.href="../index.html"
     })
-
 })(document);
 
 /*Owl Carousel whit JQuery*/
@@ -50,60 +93,8 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-/*Carga previa a mostrar contenido de página*/
-window.onload = function(){
-    $("#preload").fadeOut();
-    $("body").removeClass("hidden");
-};
 
-/*ContactForm*/
-//Se declaran anteponiendo el $ porque 
-//están haciendo referencia a un elemento del DOM
-// ((d) => {
-//     //Se declaran anteponiendo el $ porque 
-//     //están haciendo referencia a un elemento del DOM
-//     const $form = d.querySelector(".contact-form"),
-//         $loader = d.querySelector(".contact-form-loader"),
-//         $response = d.querySelector(".contact-form-response");  
 
-//         $form.addEventListener("submit", e=> {
 
-//             e.preventDefault();
-//             $loader.classList.remove("none");
-//             fetch("https://formsubmit.co/ajax/gabialberini733@gmail.com", {
-//                 method: "POST",
-//                 body: new FormData(e.target)
-//             }).then((res)=> (res.ok ? res.json() : Promise.reject(res)))
-//             .then(json =>{
-//                 console.log(json)
-//                 location.hash = "#gracias"
-//                 $form.reset();
-//             })
-//             .catch(err => {
-//                 console.log(err)
-//                 let message = err.statusText || "Ocurrió un error al enviar, intenta nuevamente"
-//                 $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`
-//             }).finally(()=> {
-//                 $loader.classList.add("none")
-//                 setTimeout(() => {
-//                     location.hash = "#close"
-//                 }, 1500)
-//             });   
-//         });
 
-// })(document)
-
- ((d) => {
-    
-    const $form = d.querySelector(".contact-form"),
-        $loader = d.querySelector(".contact-form-loader"),
-        $response = d.querySelector(".contacto-form-response");
-
-    $form.addEventListener('submit', e=> {
-        e.preventDefault();
-        $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/chefpeare@gmail.com")
-    })
-
- })(document);
 
