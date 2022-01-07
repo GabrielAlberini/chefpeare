ba74afc9da5430a597148c8fbabd2648045ea1cf
CuerpoCasa <gabialberini733@gmail.com>
2022-01-07 01:25:43 -0300

    V3.6

--- app.js
+++ app.js
@@ -83,19 +83,13 @@ const $form = d.querySelector(".contact-form"),
     $musica.addEventListener('click', () => {
         let checkMusica = $musica.checked;
         let checkMusicaIngles = $musicaIngles.checked;
-        console.log(checkMusica)
-
-        if(checkMusica === true) {
-            $sonido.innerHTML =''
-        } else if (checkMusica === false) {
-            $sonido.innerHTML = '<audio src="assets/audio-pagina.mp3" autoplay></audio>';
-        }
         
-        if(checkMusicaIngles === true) {
-            $sonido.innerHTML =''
-        } else if (checkMusicaIngles === false) {
-            $sonido.innerHTML = '<audio src="../assets/audio-pagina.mp3" autoplay></audio>';
-        }
+        checkMusica
+        ?$sonido.innerHTML ='<audio src="assets/audio-pagina.mp3" autoplay></audio>'
+        :$sonido.innerHTML = '';
+        checkMusicaIngles
+        ?$sonido.innerHTML ='<audio src="../assets/audio-pagina.mp3" autoplay></audio>'
+        :$sonido.innerHTML = '';
     })
 })(document);
 
